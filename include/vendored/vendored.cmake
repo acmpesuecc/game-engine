633de4a32502aebb30d89875739e3af7e498fc3b
Delta18-Git <64347927+Delta18-Git@users.noreply.github.com>
2025-04-23 17:35:55 +0530

    feat: add vendored dependencies management with ImGui integration
    Implement SDL3, SDL3_ttf and ImGui as vendored dependencies using
    FetchContent.

---
+++ include/vendored/vendored.cmake
@@ -0,0 +1,40 @@
+cmake_minimum_required(VERSION 3.28...4.0)
+include(FetchContent)
+
+set(BUILD_SHARED_LIBS OFF)
+set(SDL_SHARED OFF)
+set(SDL_STATIC ON)
+
+FetchContent_Declare(
+    SDL3
+    GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
+    GIT_TAG release-3.2.10
+    SOURCE_DIR ${CMAKE_SOURCE_DIR}/include/vendored/SDL3
+    EXCLUDE_FROM_ALL
+)
+FetchContent_Declare(
+    SDL3_ttf
+    GIT_REPOSITORY https://github.com/libsdl-org/SDL_ttf.git
+    GIT_TAG release-3.2.2
+    SOURCE_DIR ${CMAKE_SOURCE_DIR}/include/vendored/SDL3_ttf
+    EXCLUDE_FROM_ALL
+)
+FetchContent_MakeAvailable(SDL3 SDL3_ttf)
+FetchContent_Populate(imgui
+  URL https://github.com/ocornut/imgui/archive/master.zip
+  SOURCE_DIR ${CMAKE_SOURCE_DIR}/include/vendored/imgui
+)
+add_library(imgui_sdl3
+    STATIC
+    ${CMAKE_SOURCE_DIR}/include/vendored/imgui/imgui.cpp
+    ${CMAKE_SOURCE_DIR}/include/vendored/imgui/imgui_demo.cpp
+    ${CMAKE_SOURCE_DIR}/include/vendored/imgui/imgui_draw.cpp
+    ${CMAKE_SOURCE_DIR}/include/vendored/imgui/imgui_tables.cpp
+    ${CMAKE_SOURCE_DIR}/include/vendored/imgui/imgui_widgets.cpp
+    ${CMAKE_SOURCE_DIR}/include/vendored/imgui/backends/imgui_impl_sdl3.cpp
+    ${CMAKE_SOURCE_DIR}/include/vendored/imgui/backends/imgui_impl_sdlrenderer3.cpp
+)
+target_include_directories(imgui_sdl3 PUBLIC
+    ${CMAKE_SOURCE_DIR}/include/vendored/imgui
+    ${CMAKE_SOURCE_DIR}/include/vendored/imgui/backends
+)
