077858b21744c83a9882840163a9e6662d3d34a9
Selvaganesh Arunmozhi <thisisselva18@gmail.com>
2025-04-27 17:42:25 +0530

    modded viewport

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,17 @@ const int COLOR_BLUE = 0;
 const int COLOR_ALPHA = 128;
 
 struct AppState {
-    SDL_Window *mainWindow;
-    SDL_Renderer *mainRenderer;
+    SDL_Window* mainWindow;
+    SDL_Renderer* mainRenderer;
+    int windowWidth = SCREEN_WIDTH;
+    int windowHeight = SCREEN_HEIGHT;
     // Textures and other subsystems can be added at a later stage
     SDL_AppResult appResult = SDL_APP_CONTINUE;
 };
 
 // A simple helper function that logs that a critical failure happens and
 // returns an app failure.
-SDL_AppResult SDL_Failure(const char *fmt) {
+SDL_AppResult SDL_Failure(const char* fmt) {
     SDL_LogCritical(SDL_LOG_CATEGORY_ERROR, "%s: %s\n", fmt, SDL_GetError());
     return SDL_APP_FAILURE;
 }
@@ -30,22 +32,24 @@ SDL_AppResult SDL_AppInit(void** appState, int argc, char* argv[]) {
     AppState* state = new AppState();
     state->mainWindow = nullptr;
     state->mainRenderer = nullptr; // This only is useful this way if we are initialising
-                       // subsystems using pointers, which isn't very common. We
-                       // can instead initialise the state after all subsystems
-                       // are initialised, but this depends on which subsystems
-                       // we are initialising.
+    // subsystems using pointers, which isn't very common. We
+    // can instead initialise the state after all subsystems
+    // are initialised, but this depends on which subsystems
+    // we are initialising.
     SDL_SetAppMetadata("GameEngine", "0.0.1",
-                       "org.acm.pesuecc.aiep.game-engine");
+        "org.acm.pesuecc.aiep.game-engine");
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
         return SDL_Failure("Error initialising SDL3");
     }
-    bool initWindowRenderer{SDL_CreateWindowAndRenderer(
+
+    bool initWindowRenderer{ SDL_CreateWindowAndRenderer(
         "GameEngine", SCREEN_WIDTH, SCREEN_HEIGHT,
         SDL_WINDOW_RESIZABLE | SDL_WINDOW_TRANSPARENT, &(state->mainWindow),
-        &(state->mainRenderer))};
+        &(state->mainRenderer)) };
     if (!initWindowRenderer) {
         return SDL_Failure("Error creating Window and Renderer");
     }
+
     *appState = state;
     return SDL_APP_CONTINUE;
 }
@@ -53,20 +57,23 @@ SDL_AppResult SDL_AppInit(void** appState, int argc, char* argv[]) {
 // The event handling function called by SDL.
 // TODO: Try and make a genericsed function that is called from here to allow
 //       custom event handling to be added by a user.
-SDL_AppResult SDL_AppEvent(void *appState, SDL_Event *event) {
-    AppState *state = static_cast<AppState *>(appState);
+SDL_AppResult SDL_AppEvent(void* appState, SDL_Event* event) {
+    AppState* state = static_cast<AppState*>(appState);
     if (event->type == SDL_EVENT_QUIT || event->type == SDL_EVENT_TERMINATING) {
         state->appResult = SDL_APP_SUCCESS;
     }
     else if (event->type == SDL_EVENT_WINDOW_RESIZED) {
-        SDL_Log("Window resized to %d x %d", event->window.data1,
-            event->window.data2);
+        // Keep window size in sync when resized
+        state->windowWidth = event->window.data1;
+        state->windowHeight = event->window.data2;
+        SDL_Log("Window resized to %d x %d", state->windowWidth,
+            state->windowHeight);
     }
-    else if (event->type == SDL_EVENT_KEY_DOWN) {  
-        SDL_Log("Key pressed: %c", event->key.key);  
-    }  
-    else if (event->type == SDL_EVENT_KEY_UP) {  
-        SDL_Log("Key was released: %c", event->key.key);  
+    else if (event->type == SDL_EVENT_KEY_DOWN) {
+        SDL_Log("Key pressed: %c", event->key.key);
+    }
+    else if (event->type == SDL_EVENT_KEY_UP) {
+        SDL_Log("Key was released: %c", event->key.key);
     }
     else if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
         SDL_Log("Mouse button pressed");
@@ -79,42 +86,42 @@ SDL_AppResult SDL_AppEvent(void *appState, SDL_Event *event) {
     }
     else if (event->type == SDL_EVENT_MOUSE_MOTION) {
         SDL_Log("Mouse moved to: (%.0f, %.0f)", event->motion.x, event->motion.y);
-    }    
+    }
     return SDL_APP_CONTINUE;
 }
 
 // The "main loop" of the window.
-SDL_AppResult SDL_AppIterate(void *appState) {
-    AppState *state = static_cast<AppState *>(appState);
+SDL_AppResult SDL_AppIterate(void* appState) {
+    AppState* state = static_cast<AppState*>(appState);
 
-	//Rendering the window
-	SDL_SetRenderViewport(state->mainRenderer, NULL);
+    // Rendering the window
+    SDL_SetRenderViewport(state->mainRenderer, NULL);
     SDL_SetRenderDrawColor(state->mainRenderer, COLOR_RED, COLOR_GREEN,
-                           COLOR_BLUE, COLOR_ALPHA);
+        COLOR_BLUE, COLOR_ALPHA);
     SDL_RenderClear(state->mainRenderer);
 
-    //Top left corner viewport
+    // Top left corner viewport
     SDL_Rect topLeftViewport;
-    topLeftViewport.x = 0;
+    topLeftViewport.x = 200;
     topLeftViewport.y = 0;
-    topLeftViewport.w = SCREEN_WIDTH / 2;
-    topLeftViewport.h = SCREEN_HEIGHT / 2;
+    topLeftViewport.w = state->windowWidth / 1.5;
+    topLeftViewport.h = state->windowHeight / 1.4;
 
+    // Rendering the "partially" top left viewport
     SDL_SetRenderViewport(state->mainRenderer, &topLeftViewport);
     SDL_SetRenderDrawColor(state->mainRenderer, 255, 255, 255, 255);
-	SDL_RenderFillRect(state->mainRenderer, NULL);
+    SDL_RenderFillRect(state->mainRenderer, NULL);
 
     SDL_RenderPresent(state->mainRenderer);
     return state->appResult;
 }
 
 // Cleans up the initialised subsystems.
-void SDL_AppQuit(void *appState, SDL_AppResult result) {
-    AppState *state = static_cast<AppState *>(appState);
+void SDL_AppQuit(void* appState, SDL_AppResult result) {
+    AppState* state = static_cast<AppState*>(appState);
     if (state != nullptr) {
         SDL_DestroyRenderer(state->mainRenderer);
         SDL_DestroyWindow(state->mainWindow);
         delete state;
     }
 }
-
