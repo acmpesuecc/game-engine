30faed878d165fc643be5d92a7a2a4c6eaf3c053
Selvaganesh Arunmozhi <thisisselva18@gmail.com>
2025-05-07 23:08:46 +0530

    feat. Canvas w/ Scale Factor

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_rect.h>
 #include <SDL3/SDL_render.h>
+#include <SDL3/SDL_keyboard.h>
 
 const int SCREEN_WIDTH = 1000;
 const int SCREEN_HEIGHT = 800;
@@ -20,6 +21,9 @@ struct AppState {
     SDL_Texture* tile_map_texture;
     int windowWidth = SCREEN_WIDTH;
     int windowHeight = SCREEN_HEIGHT;
+	int offSetX = 0;
+	int offSetY = 0; 
+	float zoom = 1.0f; // Zoom factor for the canvas
 	int tilemap[100][100] = { 0 };
 	bool tilemapInitialised = false; // Just to check if the tilemap has been initialised 
     // Textures and other subsystems can be added at a later stage
@@ -55,7 +59,7 @@ SDL_AppResult SDL_AppInit(void** appState, int argc, char* argv[]) {
         return SDL_Failure("Error creating Window and Renderer");
     }
 
-    // AFTER creating renderer, NOW load tilemap
+	// Loading tilemap after creating the window and renderer
     state->tile_map = SDL_LoadBMP("./src/white_tile_32by32.bmp");
     if (!state->tile_map) {
         return SDL_Failure("Failed to load BMP for tilemap");
@@ -67,84 +71,7 @@ SDL_AppResult SDL_AppInit(void** appState, int argc, char* argv[]) {
 
     *appState = state;
     return SDL_APP_CONTINUE;
-}
-                                         
-void RenderCanvas(AppState* state) { // So everything that has to do with the canvas; the tilemap,
-                                     //  the sprites, etc. are to be included under this function
-    // Rendering the Top left corner viewport 
-    state->Canvas.x = 300;
-    state->Canvas.y = 0;
-    state->Canvas.w = state->windowWidth * 0.6;
-	state->Canvas.h = state->windowHeight * 0.75;
-
-    // Rendering the "partially" top left viewport 
-    SDL_SetRenderViewport(state->mainRenderer, &(state->Canvas));
-    SDL_SetRenderDrawColor(state->mainRenderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(state->mainRenderer, NULL);
-
-	// Rendering the tilemap
-    if (!state->tilemapInitialised) {
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                state->tilemap[i][j] = rand() % 4 + 1;
-            }
-        }
-		state->tilemapInitialised = true;
-    }
-
-    //Populating the screen with tiles
-    SDL_FRect tile[100][100];
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 100; j++) {
-            tile[i][j].x = i * 32;
-            tile[i][j].y = j * 32;
-            tile[i][j].w = 32;
-            tile[i][j].h = 32;
-        }
-    }
-
-	// Selecting the tile to be rendered
-    SDL_FRect select_tile_1;
-	select_tile_1.x = 0;
-	select_tile_1.y = 0;
-	select_tile_1.w = 32;
-	select_tile_1.h = 32;
-	SDL_FRect select_tile_2;
-	select_tile_2.x = 32;
-	select_tile_2.y = 0;
-	select_tile_2.w = 32;
-	select_tile_2.h = 32;
-	SDL_FRect select_tile_3;
-	select_tile_3.x = 0;
-	select_tile_3.y = 32;
-	select_tile_3.w = 32;
-	select_tile_3.h = 32;
-	SDL_FRect select_tile_4;
-	select_tile_4.x = 32;
-	select_tile_4.y = 32;
-	select_tile_4.w = 32;
-	select_tile_4.h = 32;
-
-	// Rendering the tilemap
-	for (int i = 0; i < 100; i++) {
-		for (int j = 0; j < 100; j++) {
-			if (state->tilemap[i][j] == 1) {
-				SDL_RenderTexture(state->mainRenderer, state->tile_map_texture, &select_tile_1, &tile[i][j]);
-			}
-			else if (state->tilemap[i][j] == 2) {
-				SDL_RenderTexture(state->mainRenderer, state->tile_map_texture, &select_tile_2, &tile[i][j]);
-			}
-			else if (state->tilemap[i][j] == 3) {
-				SDL_RenderTexture(state->mainRenderer, state->tile_map_texture, &select_tile_3, &tile[i][j]);
-			}
-			else if (state->tilemap[i][j] == 4) {
-				SDL_RenderTexture(state->mainRenderer, state->tile_map_texture, &select_tile_4, &tile[i][j]);
-			}
-		}
-	}
-
-}
-
+}     
 
 // The event handling function called by SDL.
 // TODO: Try and make a genericsed function that is called from here to allow
@@ -182,9 +109,123 @@ SDL_AppResult SDL_AppEvent(void* appState, SDL_Event* event) {
     return SDL_APP_CONTINUE;
 }
 
+static void RenderCanvas(AppState* state) { // So everything that has to do with the canvas; the tilemap,
+    //  the sprites, etc. are to be included under this function
+// Rendering the Top left corner viewport 
+    state->Canvas.x = 300;
+    state->Canvas.y = 0;
+    state->Canvas.w = state->windowWidth / 1.5625;
+    state->Canvas.h = state->windowHeight / 1.6667;
+
+    // Rendering the "partially" top left viewport 
+    SDL_SetRenderViewport(state->mainRenderer, &(state->Canvas));
+    SDL_SetRenderDrawColor(state->mainRenderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(state->mainRenderer, NULL);
+
+    // Rendering the tilemap
+    if (!state->tilemapInitialised) {
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                state->tilemap[i][j] = rand() % 4 + 1;
+            }
+        }
+        state->tilemapInitialised = true;
+    }
+
+    //Populating the screen with tiles
+    SDL_FRect tile[100][100];
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            tile[i][j].x = (i * 32) - state->offSetX;
+            tile[i][j].y = (j * 32) - state->offSetY;
+            tile[i][j].w = 32.0f * state->zoom;
+            tile[i][j].h = 32.0f * state->zoom;
+        }
+    }
+
+    // Selecting the tile to be rendered
+    SDL_FRect select_tile_1;
+    select_tile_1.x = 0;
+    select_tile_1.y = 0;
+    select_tile_1.w = 32;
+    select_tile_1.h = 32;
+    SDL_FRect select_tile_2; 
+    select_tile_2.x = 32;
+    select_tile_2.y = 0;
+    select_tile_2.w = 32;
+    select_tile_2.h = 32;
+    SDL_FRect select_tile_3;
+    select_tile_3.x = 0;
+    select_tile_3.y = 32;
+    select_tile_3.w = 32;
+    select_tile_3.h = 32;
+    SDL_FRect select_tile_4;
+    select_tile_4.x = 32;
+    select_tile_4.y = 32;
+    select_tile_4.w = 32;
+    select_tile_4.h = 32;
+
+    // Rendering the tilemap
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            if (state->tilemap[i][j] == 1) {
+                SDL_RenderTexture(state->mainRenderer, state->tile_map_texture, &select_tile_1, &tile[i][j]);
+            }
+            else if (state->tilemap[i][j] == 2) {
+                SDL_RenderTexture(state->mainRenderer, state->tile_map_texture, &select_tile_2, &tile[i][j]);
+            }
+            else if (state->tilemap[i][j] == 3) {
+                SDL_RenderTexture(state->mainRenderer, state->tile_map_texture, &select_tile_3, &tile[i][j]);
+            }
+            else if (state->tilemap[i][j] == 4) {
+                SDL_RenderTexture(state->mainRenderer, state->tile_map_texture, &select_tile_4, &tile[i][j]);
+            }
+        }
+    }
+}
+
 // The "main loop" of the window.
 SDL_AppResult SDL_AppIterate(void* appState) {
     AppState* state = static_cast<AppState*>(appState);
+
+    //Handling keyboard inputs
+    const bool* keys = SDL_GetKeyboardState(NULL);
+    if (keys[SDL_SCANCODE_LEFT]) {
+        state->offSetX -= 5;
+    }
+    if (keys[SDL_SCANCODE_RIGHT]) {
+        state->offSetX += 5;
+    }
+    if (keys[SDL_SCANCODE_UP]) {
+        state->offSetY -= 5;
+    }
+    if (keys[SDL_SCANCODE_DOWN]) {
+        state->offSetY += 5;
+    }
+    if (keys[SDL_SCANCODE_MINUS]) {
+		state->zoom *= 0.95f;
+        if (state->zoom < 0.05f) {
+			state->zoom = 0.05f;
+        }
+    }
+	if (keys[SDL_SCANCODE_EQUALS]) {
+		state->zoom *= 1.05f;
+		if (state->zoom > 5.0f) {
+			state->zoom = 5.0f;
+		}
+	}
+	if (state->offSetX < 0) {
+		state->offSetX = 0;
+	}
+	if (state->offSetY < 0) {
+		state->offSetY = 0;
+	}
+	if (state->offSetX > 1400) {
+		state->offSetX = 1400;
+	}
+	if (state->offSetY > 1800) {
+		state->offSetY = 1800;
+	}
 
     // Rendering the window
     SDL_SetRenderViewport(state->mainRenderer, NULL);
